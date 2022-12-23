b148b724e94bd77068b265a23c04d9e7751ca7a3
azizyilmazcetin <73158875+azizyilmazcetin@users.noreply.github.com>
2022-12-24 01:12:29 +0300

    Add files via upload

---
+++ src/app/component/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  email : string = '';
+  password : string = '';
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  register() {
+    if(this.email == ''){
+      alert('Lütfen E-Posta Adresinizi Girin');
+      return;
+    }
+    if(this.password == ''){
+      alert("Lütfen Şifrenizi Giriniz");
+      return;
+    }
+    this.auth.register(this.email,this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'angular-login-2851e',
+    appId: '1:198930269380:web:bc2033b586181eafb8b591',
+    databaseURL: 'https://angular-login-2851e-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'angular-login-2851e.appspot.com',
+    apiKey: 'AIzaSyCm04k8RyUmNhMpNPmUy6e6ymgUAXAfrtg',
+    authDomain: 'angular-login-2851e.firebaseapp.com',
+    messagingSenderId: '198930269380',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
